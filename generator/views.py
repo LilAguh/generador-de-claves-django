b4415f534b2008426c504e6f1f869a516c133783
LilAguh <aguezeochoa@gmail.com>
2023-04-16 18:26:17 -0300

    se crea la logica y el renderizado para las contraseñas

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+import random
+# from django.http import HttpResponse
 
-# Create your views here.
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    generated_password = ''
+    length = int(request.GET.get('length'))
+    uppercase = request.GET.get('uppercase')
+    special = request.GET.get('special')
+    numbers = request.GET.get('numbers')
+
+    if special:
+        characters.extend(list('@!#$%&?¡¿*+-_'))
+
+    if uppercase:
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if numbers:
+        characters.extend(list('1234567890'))
+
+    for x in range(length):
+        generated_password += random.choice(characters)
+
+    return render(request, 'password.html', {'password': generated_password})
